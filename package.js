c8f54258efb97389bf6ec4915984620e4119b20a
Dominique Quatravaux <dominique@quatravaux.org>
2016-03-16 15:28:49 +0100

    Bump version
    Re-push after merge

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'epfl:loginfirst',
-  version: '0.0.1',
+  version: '0.0.2',
   // Brief, one-line summary of the package.
   summary: 'Queue all DDP messages until the user has logged in',
   // URL to the Git repository containing the source code for this package.
