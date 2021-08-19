a5b5938dbbc607d66720e01abe7c8acbefcf5f18
Dominique Quatravaux <dominique.quatravaux@epfl.ch>
2021-08-19 16:03:08 +0200

    [test] Put forth a test plan
    As per https://mochajs.org/#arrow-functions, don't use arrow functions.

--- tests/server-tests.js
+++ tests/server-tests.js
@@ -1,3 +1,13 @@
-describe("Server-side tests", () => {
-  it("pwns!")
+describe("Server-side tests", function() {
+  describe("Logged-out state", function() {
+    it("lets the client call `login()`")
+    it("lets the client subscribe to `meteor.loginServiceConfiguration`")
+    it("blocks other subscriptions")
+    it("blocks other methods")
+  })
+
+  describe("Logged-in state", function() {
+    it("lets the client call any method")
+    it("lets the client subscribe to anything")
+  })
 })
